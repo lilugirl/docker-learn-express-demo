7ac981026407ef1feadd36264c02697b766cc02a
liu yi <lilugirl2005@126.com>
2021-10-31 22:39:40 +0800

    创建一个博客的增删改查api

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const {MONGO_USER,MONGO_PASSWORD,MONGO_IP,MONGO_PORT}=require("./config/config");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
 const app = express();
 
-const mongoURL=`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/admin`;
-const connectWithRetry = () =>{
-  mongoose.connect(mongoURL, {
+const postRouter=require('./routes/postRoutes');
 
-  }).then(() => console.log("successfully connect to DB"))
-  .catch((e) => {
-    console.log(e)
-    setTimeout(connectWithRetry, 5000)
-  });
-}
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/admin`;
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL, {})
+    .then(() => console.log("successfully connect to DB"))
+    .catch((e) => {
+      console.log(e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
 
-connectWithRetry()
+connectWithRetry();
 
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h2>Hi There!！</h2>");
 });
+
+app.use("/api/v1/posts",postRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
